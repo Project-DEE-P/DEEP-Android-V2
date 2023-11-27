5277d15676cb03185bb79403bb634299eaeb9599
stev3j <mygajeong@gmail.com>
2023-11-28 01:55:49 +0900

    [SET] set setting logic

--- presentation/src/main/java/com/dragonest/deep_v2/feature/setting/SettingFragment.kt
+++ presentation/src/main/java/com/dragonest/deep_v2/feature/setting/SettingFragment.kt
@@ -5,9 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.dragonest.deep_v2.R
+import com.dragonest.deep_v2.databinding.FragmentSettingBinding
+import com.dragonest.deep_v2.util.DeepApplication
 import com.ggd.zendee.base.BaseFragment
 
-class SettingFragment: BaseFragment<>() {
+class SettingFragment: BaseFragment<FragmentSettingBinding, SettingViewModel>(R.layout.fragment_setting) {
+
+    override val viewModel: SettingViewModel by viewModels()
+
+    override fun start() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.btnLogout.setOnClickListener {
+            DeepApplication.prefs.deleteToken()
+            findNavController().navigate(SettingFragmentDirections.toStartFragment())
+        }
+    }
 
 }
